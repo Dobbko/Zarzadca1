a62f1de4d6f1eb8d58291d39e4907d3a6b31a20d
Dobbko <66557487+Dobbko@users.noreply.github.com>
2021-09-17 18:53:40 +0200

    Delete src/app directory

--- src/app/autoryzacja.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AutoryzacjaDirective } from './autoryzacja.directive';
-
-describe('AutoryzacjaDirective', () => {
-  it('should create an instance', () => {
-    const directive = new AutoryzacjaDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/app/czerwony-tekst.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CzerwonyTekstDirective } from './czerwony-tekst.directive';
-
-describe('CzerwonyTekstDirective', () => {
-  it('should create an instance', () => {
-    const directive = new CzerwonyTekstDirective();
-    expect(directive).toBeTruthy();
-  });
-});
